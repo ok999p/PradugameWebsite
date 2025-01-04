b43d0a16c086a63d9b118dd27f4f30661d97d8b4
ok999p <tachanon.srisook@gmail.com>
2025-01-04 16:03:07 +0700

    Test2

--- Pradu/resources/views/welcome.blade.php
+++ Pradu/resources/views/welcome.blade.php
@@ -4,43 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hello</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset(path: 'css/style.css') }}">
 </head>
 <body>
-<div class="w-full h-screen">
-    <div>
-        <img src="{{ asset('/Logo.png') }}" alt="" style="width: 100px">
-        </div>
-    </div>
     <div class="load" id="preloader">
         <div class="progress">
-            <img src="{{ asset('/img/Logo_6.png') }}" alt="" class="w-1/2" style="width: 60px">
+            <img src="{{ asset('/img/Logo_6.svg') }}" alt="Logo_6" style="width: 60px">
         </div>
         <div class="progress">
-            <img src="{{ asset('/img/Logo_5.png') }}" alt="" class="w-1/2" style="width: 60px">
+            <img src="{{ asset('/img/Logo_5.svg') }}" alt="Logo_5" style="width: 60px">
         </div>
         <div class="progress">
-            <img src="{{ asset('/img/Logo_4.png') }}" alt="" class="w-1/2" style="width: 60px">
+            <img src="{{ asset('/img/Logo_4.svg') }}" alt="Logo_4" style="width: 60px">
         </div>
         <div class="progress">
-            <img src="{{ asset('/img/Logo_3.png') }}" alt="" class="w-1/2" style="width: 60px">
+            <img src="{{ asset('/img/Logo_3.svg') }}" alt="Logo_3" style="width: 60px">
         </div>
         <div class="progress">
-            <img src="{{ asset('/img/Logo_2.png') }}" alt="" class="w-1/2" style="width: 60px">
+            <img src="{{ asset('/img/Logo_2.svg') }}" alt="Logo_2" style="width: 60px">
         </div>
         <div class="progress">
-            <img src="{{ asset('/img/Logo_1.png') }}" alt="" class="w-1/2" style="width: 60px">
+            <img src="{{ asset('/img/Logo_1.svg') }}" alt="Logo_1" style="width: 60px">
         </div>
     </div>
 
 
     <script>
-        // window.addEventListener('load', function () {
-        //     const loader = document.getElementById('preloader');
-        //     setTimeout(() => {
-        //         loader.style.display = 'none';
-        //     }, 2000);
-        // });
-    </script>
+   window.addEventListener('load', function () {
+        const loader = document.getElementById('preloader');
+        setTimeout(() => {
+            loader.style.display = 'none'; // ซ่อน preloader หลังจาก delay 2 วินาที
+        }, 2300); // หน่วงเวลา 2 วินาที (2000ms)
+    });
+</script>
+
 </body>
 </html>
